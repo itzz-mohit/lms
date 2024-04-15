4edad3c226ba2bc66ca72511f7e3f11d8bf8f4d6
Mohit <mohitbite@gmail.com>
2024-04-15 12:30:54 +0530

    first commit

---
+++ client/src/components/authentication/Register.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+
+const Register = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <section className="mt-14">
+      <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
+        <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
+          <h2 className="text-center text-4xl font-bold leading-tight text-black">
+            Sign Up
+          </h2>
+
+          <form className="mt-8" onSubmit={handleSubmit}>
+            <div className="space-y-5">
+              <div>
+                <label
+                  htmlFor=""
+                  className="text-base font-medium text-gray-900"
+                >
+                  Name
+                </label>
+                <div className="mt-2">
+                  <input
+                    className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                    type="name"
+                    placeholder="Name"
+                  ></input>
+                </div>
+              </div>
+
+              <div>
+                <label
+                  htmlFor=""
+                  className="text-base font-medium text-gray-900"
+                >
+                  Email address
+                </label>
+                <div className="mt-2">
+                  <input
+                    className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                    type="email"
+                    placeholder="Email"
+                  ></input>
+                </div>
+              </div>
+              <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    htmlFor=""
+                    className="text-base font-medium text-gray-900"
+                  >
+                    Password
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                    type="password"
+                    placeholder="Password"
+                  ></input>
+                </div>
+              </div>
+              <p className="mt-2  text-sm text-gray-600 ">
+                Already have an account?
+                <span className="font-semibold text-black transition-all duration-200 hover:underline ms-1 cursor-pointer">
+                  Sign In
+                </span>
+              </p>
+              <div>
+                <button
+                  type="submit"
+                  className="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
+                >
+                  Register
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Register;
